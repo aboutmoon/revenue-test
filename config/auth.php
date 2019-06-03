6ef9cae525ca9d9110c83681acc786b9feeff524
biao.he@kaiostech.com <biao.he@kaiostech.com>
2019-06-03 14:09:40 +0800

    创建会话

--- config/auth.php
+++ config/auth.php
@@ -68,7 +68,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\Models\User\Models::class,
+            'model' => App\Models\User::class,
         ],
 
         // 'users' => [
