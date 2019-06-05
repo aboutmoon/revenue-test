8ad5dcb810a9c659b06300f15863a9999c5f78ab
biao.he@kaiostech.com <biao.he@kaiostech.com>
2019-06-05 13:20:28 +0800

    微博数据模型

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,47 +10,52 @@ use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+  use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = [
+    'name', 'email', 'password',
+  ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+  /**
+   * The attributes that should be hidden for arrays.
+   *
+   * @var array
+   */
+  protected $hidden = [
+    'password', 'remember_token',
+  ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+  /**
+   * The attributes that should be cast to native types.
+   *
+   * @var array
+   */
+  protected $casts = [
+    'email_verified_at' => 'datetime',
+  ];
 
-    public function gravatar($size = '100')
-    {
-      $hash = md5(strtolower(trim($this->attributes['email'])));
-      return "http://www.gravatar.com/avatar/$hash?s=$size";
-    }
+  public function gravatar($size = '100')
+  {
+    $hash = md5(strtolower(trim($this->attributes['email'])));
+    return "http://www.gravatar.com/avatar/$hash?s=$size";
+  }
 
-    public static function boot()
-    {
-      parent::boot();
+  public static function boot()
+  {
+    parent::boot();
 
-      static::creating(function ($user) {
-        $user->activation_token = Str::random(10);
-      });
-    }
+    static::creating(function ($user) {
+      $user->activation_token = Str::random(10);
+    });
+  }
+
+  public function statuses()
+  {
+    return $this->hasMany(Status::class);
+  }
 }
