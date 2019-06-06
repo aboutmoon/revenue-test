d183f7d732018641acdbb059b0a800f4f672d856
biao.he@kaiostech.com <biao.he@kaiostech.com>
2019-06-06 15:30:51 +0800

    关注按钮

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -7,15 +7,20 @@ use App\Models\User;
 
 class UserPolicy
 {
-  use HandlesAuthorization;
+    use HandlesAuthorization;
 
-  public function update(User $currentUser, User $user)
-  {
-    return $currentUser->id === $user->id;
-  }
+    public function update(User $currentUser, User $user)
+    {
+        return $currentUser->id === $user->id;
+    }
 
-  public function destroy(User $currentUser, User $user)
-  {
-    return $currentUser->is_admin && $currentUser->id !== $user->id;
-  }
+    public function destroy(User $currentUser, User $user)
+    {
+        return $currentUser->is_admin && $currentUser->id !== $user->id;
+    }
+
+    public function follow(User $currentUser, User $user)
+    {
+        return $currentUser->id !== $user->id;
+    }
 }
